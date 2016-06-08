e7abcc1ba09f6663d72f4353a978ea05c9450268
Jaime Ambrosio <Jaime Ambrosio@JaimeAmbrosio>
2016-06-08 01:20:21 -0500

    Nuevo Commit

--- web/js/config-ini.js
+++ web/js/config-ini.js
@@ -1,10 +1,15 @@
 
 function mostrarModalMensaje(mensaje, detalle, tipo)
 {
+    if (detalle === undefined || detalle === null)
+        detalle = "Sin detalle.";
+    if (mensaje === undefined || mensaje === null)
+        mensaje = "Sin mensaje.";
+
     $("#modalMensajesIconInformation").hide();
     $("#modalMensajesIconWarning").hide();
     $("#collapseModalMensajeDetalle").collapse("hide");
-    
+
     if (tipo == "INFORMACION") {
         var titulo = '<span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Información';
         $("#modalMensajesIconInformation").show();
@@ -14,16 +19,15 @@ function mostrarModalMensaje(mensaje, detalle, tipo)
         $("#modalMensajesIconWarning").show();
         $("#modalMensajeTitle").html(titulo);
     } else {
-
+        
     }
     $("#modalMensajesCuerpo").html(mensaje);
-    /*
+    
     var i = detalle.search("<body>");
     if (i > -1) {
         var j = detalle.search("</body>");
         detalle = detalle.substring(i, j);
     }
-    */
     $("#modalMensajesDetalle").html(detalle);
     $('#modalMensajes').modal('show');
 }
@@ -47,6 +51,7 @@ $().ready(function () {
                                     "ERROR");
                         }
                         NProgress.done();
+                        console.log(xhr);
                     });
 
                 });
@@ -60,7 +65,7 @@ $().ready(function () {
 function prueba() {
     // NProgress.start();
     //mostrarModalMensaje('adadad', '<!DOCTYPE html><html><head><title>Apache Tomcat/8.0.27 - Informe de Error</title><style type="text/css">H1 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:22px;} H2 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:16px;} H3 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:14px;} BODY {font-family:Tahoma,Arial,sans-serif;color:black;background-color:white;} B {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;} P {font-family:Tahoma,Arial,sans-serif;background:white;color:black;font-size:12px;}A {color : black;}A.name {color : black;}.line {height: 1px; background-color: #525D76; border: none;}</style> </head><body><h1>Estado HTTP 404 - /AppFinanzas/paginas/paneles/p-miperfill.jsp</h1><div class="line"></div><p><b>type</b> Informe de estado</p><p><b>mensaje</b> <u>/AppFinanzas/paginas/paneles/p-miperfill.jsp</u></p><p><b>descripción</b> <u>El recurso requerido no está disponible.</u></p><hr class="line"><h3>Apache Tomcat/8.0.27</h3></body></html>',"INFORMACION");
-    mostrarModalMensaje('The following example makes a button toggle the expanding and collapsing content of another element', 'The following example makes a button toggle the expanding and collapsing content of another element',"ERROR");
+    mostrarModalMensaje('The following example makes a button toggle the expanding and collapsing content of another element', 'The following example makes a button toggle the expanding and collapsing content of another element', "ERROR");
     // NProgress.done();
 }
 
